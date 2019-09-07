76d09071f7ef7fd6d16096c0441a915c55b69524
offorma <offorma@gmail.com>
2019-09-07 13:29:56 +0100

    added linter

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ window.onload = () => {
     loadHome();
 }
 const home = document.getElementById('home')
-home.addEventListener('click', ()=>{loadHome()});
+home.addEventListener('click', () => {loadHome()});
 
 const about = document.getElementById('about')
-about.addEventListener('click', ()=>{loadAbout()});
+about.addEventListener('click', () => {loadAbout()});
 
 const menus = document.getElementById('contact')
-menus.addEventListener('click', ()=>{loadContact()});
+menus.addEventListener('click', () => {loadContact()});
 
 
