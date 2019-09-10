f5ab27f05f93944670b062edc5c3b9561c0dcd84
offorma <offorma@gmail.com>
2019-09-10 09:03:13 +0100

    ignored main.js in sticker config

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import {loadContact} from './contact'
 import {loadAbout} from './aboutcontent'
 
 
+
+const home = document.getElementById('home')
+home.addEventListener('click',loadHome());
+
+const about = document.getElementById('about')
+about.addEventListener('click', loadAbout());
+
+const menus = document.getElementById('contact')
+menus.addEventListener('click', loadContact);
+
 window.onload = () => {
     loadHome();
 }
-const home = document.getElementById('home')
-home.addEventListener('click', () => {loadHome()});
-
-const about = document.getElementById('about')
-about.addEventListener('click', () => {loadAbout()});
-
-const menus = document.getElementById('contact')
-menus.addEventListener('click', () => {loadContact()});
-
-
