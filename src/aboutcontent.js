b02f39494fd51346a168f221505d0c55007a51da
offorma <offorma@gmail.com>
2019-09-16 13:14:00 +0100

    fixing lint errors

--- src/aboutcontent.js
+++ src/aboutcontent.js
@@ -3,7 +3,7 @@ const aboutContent = {
 <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>`,
 };
 const loadAbout = () =>{ 
-  const content = document.getElementById("content");
+  const content = document.getElementById('content');
   content.innerHTML = '';
   content.innerHTML = aboutContent.about;
 };
