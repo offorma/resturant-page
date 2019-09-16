e29684bece0fb6d7d8a2df01e293c97fe1856da1
offorma <offorma@gmail.com>
2019-09-16 13:36:46 +0100

    fixing lint errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import {loadHome} from './homecontent'
-import {loadContact} from './contact'
-import {loadAbout} from './aboutcontent'
+import { loadHome } from './homecontent';
+import { loadContact } from './contact';
+import { loadAbout } from './aboutcontent';
 
-
-
-const home = document.getElementById('home')
+const home = document.getElementById('home');
 home.addEventListener('click',loadHome());
 
-const about = document.getElementById('about')
+const about = document.getElementById('about');
 about.addEventListener('click', loadAbout());
 
-const menus = document.getElementById('contact')
+const menus = document.getElementById('contact');
 menus.addEventListener('click', loadContact);
 
 window.onload = () => {
